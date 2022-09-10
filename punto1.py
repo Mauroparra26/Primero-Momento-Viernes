bfd0623b23ae49e33e15dfa5294c315a640170f9
209 <209@academicos.cesde.edu.co>
2022-09-09 19:02:49 -0500

    PMNTV Se crea el white

---
+++ punto1.py
@@ -0,0 +1,39 @@
+i=None
+centinela=0
+cantidad=int(input("Digite cuantos numeros desea ingresar "))
+numeros={}
+multiplos2=[]
+multiplos3=[]
+while(centinela!=cantidad):
+    i = input('Digite un número: ')
+    if (i/2) == 0: 
+      multiplos2.append(i)
+
+            
+
+        precioFabricacion = float(input('Digite el precio de fabricacion: '))
+
+        precioVenta  = float(input('Digite el precio de Venta: '))
+
+        empanada['Nombre']= nombre
+
+        empanada['Ingredientes'] = ingredientes
+
+        empanada['Precio Fabricacion']= precioFabricacion
+
+        empanada['Precio Venta']= precioVenta
+        print('Empanada guardada exitosamente')
+
+        empanadas.append(empanada)
+
+    elif eleccion == 2:
+
+        print(empanadas)
+    elif eleccion== 3:
+
+        print('Gracias por usar el Menu de empanadas')
+        break
+    else: 
+        print("Digite una opcion correcta ")
+else: 
+    print('Bye') 
